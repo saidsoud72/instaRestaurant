32862e4c93cc8a7d52102c56c9bc6b06d0e3bcbf
saidsoud72 <said.omar@student.moringaschool.com>
2022-12-08 11:51:17 +0300

    updated usernames, emails an dprofile pic.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,36 @@
+class UsersController < ApplicationController
+  skip_before_action :authorize, only: [:create]
+
+  def create
+    user = User.create!(user_params)
+    session[:user_id] = user.id
+    render json: user, status: :created
+  end
+
+  def show
+    render json: @current_user
+  end
+
+  def update
+    if @current_user
+    @current_user.update!(update_params)
+    render json: @current_user, status: :accepted
+    end
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+    render json: user, status: :no_content
+  end
+
+  private
+
+  def user_params
+    params.permit(:username, :email, :password, :password_confirmation, :profile_pic, :followers)
+  end
+
+  def update_params
+    params.permit(:username, :email, :profile_pic)
+  end
+end
